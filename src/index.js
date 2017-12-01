933db262c8feeba242ccd4085fec69486fd26258
Tom Payne <thomas.payne@skybettingandgaming.com>
2017-12-01 15:54:03 +0000

    Refactored for brevity

--- src/index.js
+++ src/index.js
@@ -4,23 +4,20 @@ const bindStateToSelector = (prevKey, selector) => (state, ...args) => {
 };
 
 const __combineSelectors = (selectors, prevKey = false) => {
-  const selectorKeys = Object.keys(selectors);
-
-  const finalSelectors = {};
-  for (let i = 0; i < selectorKeys.length; i++) {
-    const selectorKey = selectorKeys[i];
-
+  return Object.keys(selectors).reduce((finalSelectors, selectorKey) => {
     const selector = selectors[selectorKey];
     if (typeof selector === "function") {
-      finalSelectors[selectorKey] = bindStateToSelector(prevKey, selector);
+      return {
+        ...finalSelectors,
+        [selectorKey]: bindStateToSelector(prevKey, selector)
+      }
     } else {
-      finalSelectors[selectorKey] = __combineSelectors(
-        selector,
-        prevKey || selectorKey
-      );
+      return {
+        ...finalSelectors,
+        [selectorKey]: __combineSelectors( selector, prevKey || selectorKey)
+      }
     }
-  }
-  return finalSelectors;
+  }, {});
 };
 
 export const combineSelectors = modules => {
